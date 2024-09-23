b4ead1224e4b4d7675d7f34aca0da88e77917daa
Илья <iliya168@yandex.ru>
2024-09-24 02:17:29 +0300

    END

--- src/main/java/com/github/IliyaBarbarossa/servlet/MainServlet.java
+++ src/main/java/com/github/IliyaBarbarossa/servlet/MainServlet.java
@@ -13,7 +13,7 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Hello!");
+        printWriter.write("Helloovin!");
         printWriter.close();
     }
 }
