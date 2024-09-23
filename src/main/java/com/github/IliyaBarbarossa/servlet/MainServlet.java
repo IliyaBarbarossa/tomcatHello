aaa3ef3d4f41692464587a256cfe36f2e7e97b7e
Илья <iliya168@yandex.ru>
2024-09-24 00:44:05 +0300

    servlet 4.0

--- src/main/java/com/github/IliyaBarbarossa/servlet/MainServlet.java
+++ src/main/java/com/github/IliyaBarbarossa/servlet/MainServlet.java
@@ -1,18 +1,16 @@
 package com.github.IliyaBarbarossa.servlet;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class MainServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
         printWriter.write("Hello!");
